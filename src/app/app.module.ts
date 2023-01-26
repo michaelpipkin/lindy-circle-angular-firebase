a13f396e6e64868e6d376fba37de1baa67b82bbb
Pipkin, Michael <michael.a.pipkin@gmail.com>
2023-01-26 17:09:44 -0500

    Add angular firestore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
-
 
 @NgModule({
   declarations: [AppComponent],
