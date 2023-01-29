63923d68f2e7f969bfc244663aac43f949b2bebe
Pipkin, Michael <michael.a.pipkin@gmail.com>
2023-01-29 15:19:47 -0500

    Add custom style sheet, add-member component

--- src/app/members/add-member/add-member.component.ts
+++ src/app/members/add-member/add-member.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Member } from '@models/member';
+import { MembersService } from '@services/members.service';
 
 @Component({
   selector: 'app-add-member',
   templateUrl: './add-member.component.html',
-  styleUrls: ['./add-member.component.scss']
+  styleUrls: ['./add-member.component.scss'],
 })
 export class AddMemberComponent {
+  newMemberForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+  });
 
+  constructor(
+    private fb: FormBuilder,
+    private membersService: MembersService,
+    private router: Router
+  ) {}
+
+  onSubmit(): void {
+    const val = this.newMemberForm.value;
+    const newMember: Partial<Member> = {
+      firstName: val.firstName,
+      lastName: val.lastName,
+      active: true,
+    };
+  }
 }
--- src/app/services/members.service.ts
+++ src/app/services/members.service.ts
@@ -30,4 +30,6 @@ export class MembersService {
           );
         })
       );
+
+  addMember(newMember: Partial<Member>) {}
 }
